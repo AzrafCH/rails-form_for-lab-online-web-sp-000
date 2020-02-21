cc4f096cb2fd2feb3814e1d0b9d0932629e058a4
Azraf Chowdhury <achowdhur@ltu.edu>
2020-02-21 08:10:36 +0000

    Automatically backed up by Learn

---
+++ app/controllers/schoolclass_controller.rb
@@ -0,0 +1,27 @@
+class SchoolclassController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+end
